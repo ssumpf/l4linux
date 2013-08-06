8b7afabeb37c3dc04090df17267b8d6acad0ca28
Sebastian Sumpf <sebastian.sumpf@genode-labs.com>
2013-08-06 16:10:24 +0200

    L4Linux: Solve alignment issues in ICMP on ARM
    'icmp_hdr' returns a two byte aligned pointer, which will usually clash on ARM
    when the compiler generates 'ldmia' instructions. We don't have an instructions
    emulator (unlike Linux).

--- net/ipv4/icmp.c
+++ net/ipv4/icmp.c
@@ -787,7 +787,7 @@ static void icmp_echo(struct sk_buff *skb)
 	if (!net->ipv4.sysctl_icmp_echo_ignore_all) {
 		struct icmp_bxm icmp_param;
 
-		icmp_param.data.icmph	   = *icmp_hdr(skb);
+		memcpy(&icmp_param.data.icmph, icmp_hdr(skb), sizeof(struct icmphdr));
 		icmp_param.data.icmph.type = ICMP_ECHOREPLY;
 		icmp_param.skb		   = skb;
 		icmp_param.offset	   = 0;
